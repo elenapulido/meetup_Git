de9daeb94cb567a661940067ba5d3baf68471130
elenapulido <119506074+elenapulido@users.noreply.github.com>
2023-05-12 18:26:50 +0200

    navbar

--- meetup/src/routes/Routes.jsx
+++ meetup/src/routes/Routes.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Routes, Route } from "react-router-dom";
 import Commands from "../views/Commands";
 import GoodPractices from "../views/GoodPractices";
 import Mosaic from "../views/Mosaic";
+import Home from "../views/Home"
 
 
 
