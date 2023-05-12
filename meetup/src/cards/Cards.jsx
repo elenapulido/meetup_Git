846de4519a899091472f36c14334322565492e12
lucia <luciatiradoturiel@gmail.com>
2023-05-12 19:35:12 +0200

    cards

--- meetup/src/cards/Cards.jsx
+++ meetup/src/cards/Cards.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import image from "../assets/cartas-01.jpg"
+import image2 from "../assets/cartas-02.jpg"
+import image3 from "../assets/cartas-03.jpg"
 
-function cards() {
+
+function Cards() {
   return (
-    <div>cards</div>
-  )
+    <div className="d-flex justify-content-between">
+      <img src={image} alt="imagen1" />
+      <img src={image2} alt="imagen2" />
+      <img src={image3} alt="imagen3" />
+    </div>
+  );
 }
 
-export default cards
+export default Cards
--- meetup/src/views/Home.jsx
+++ meetup/src/views/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../navbar/Navbar'
+import Cards from '../cards/Cards'
 
 function Home() {
   return (
     <div>
       <Navbar />
+      <Cards />
     </div>
   )
 }
