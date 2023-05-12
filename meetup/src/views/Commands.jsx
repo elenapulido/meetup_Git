405af3aa3befe1856f9f4bad1a8883a448f30924
elenapulido <119506074+elenapulido@users.noreply.github.com>
2023-05-12 17:12:44 +0200

    change

---
+++ meetup/src/views/Commands.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Commands() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Commands
---
+++ meetup/src/views/GoodPractices.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function GoodPractices() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default GoodPractices
