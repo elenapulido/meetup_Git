6b3fe8fbb2e8346a6b97cc2af7197a61d127d275
lucia <luciatiradoturiel@gmail.com>
2023-05-15 16:29:29 +0200

    cards

--- meetup/src/cards/Cards.jsx
+++ meetup/src/cards/Cards.jsx
@@ -6,7 +6,7 @@ import image3 from "../assets/cartas-03.jpg"
 
 function Cards() {
   return (
-    <div className="d-flex justify-content-aroundgit m-4">
+    <div className="d-flex justify-content-around m-4">
       <img src={image3} alt="imagen3"style={{ width: "300px", height: "300px" }} />
       <img src={image2} alt="imagen2" style={{ width: "300px", height: "300px" }} />
       <img src={image} alt="imagen" style={{ width: "300px", height: "300px" }}/>
